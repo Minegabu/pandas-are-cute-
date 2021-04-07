ed236ad0dcdccb60aa2dbea591f48f080ab12429
Minegabu <gabriel.montelle9@gmail.com>
2021-04-08 11:37:15 +1200

    Search bar works now yay!

--- api.py
+++ api.py
@@ -22,21 +22,13 @@ def getting_stats():
 
 @app.route('/')
 def home():
-    return render_template("home.html") 
+    return render_template("home.html")
+
+@app.route('/ingamename',methods=["get","POST"])
+def data():
+    print(request.form["ingamename"])
+    print ("Gabriel's Gay")
+    return render_template("contents.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-@app.route('/data/<ingamename>')
-def data():
-    while 1:
-        try:
-            uuid = requests.get("https://api.mojang.com/users/profiles/minecraft/" +ingamename).json()
-            print(uuid['id'])
-            data = requests.get("https://api.hypixel.net/player?key=53c394a2-94ee-4549-aafd-5e8aa1227188&uuid="+uuid["id"]).json()
-            skywarsWins = data["player"]["stats"]["SkyWars"]["wins"]
-            bedwarsWins = data["player"]["stats"]["Bedwars"]["wins_bedwars"]
-            print("You have this many skywars wins:" + str(skywarsWins))
-            print("You have this many bedwars wins:" + str(bedwarsWins))
-        except:
-            print("Did you put the wrong username?")
