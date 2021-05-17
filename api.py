1bb3a28d4c06fa1d7cccc4d0b289cf6e88dc410f
Minegabu <gabriel.montelle9@gmail.com>
2021-05-18 09:08:40 +1200

    Made the search bar give input into python.

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 form = cgi.FieldStorage()
 searchterm =  form.getvalue('searchbox')
 DATABASE = "Pandas-are-cute.db"
-key = "4e67ce31-eac7-4daf-b7f7-390d7bf699b0"
+key = "e84267a6-e22c-4f9d-8e5d-7e162e6dc79b"
 
 
 
@@ -24,38 +24,38 @@ def get_db():
 def decimal_str(x: float, decimals: int = 10) -> str:
     return format(x, f".{decimals}f").lstrip().rstrip('0')
 
-def getting_stats():
-    while 1:
-        try:
-            uuid = requests.get("https://api.mojang.com/users/profiles/minecraft/" +ingamename).json()
-            print(uuid['id'])
-            data = requests.get("https://api.hypixel.net/player?key=5dc79cc8-f597-4eb8-b8a9-f22cf281c2eeuuid="+uuid["id"]).json()
-            skywarsWins = data["player"]["stats"]["SkyWars"]["wins"]
-            bedwarsWins = data["player"]["stats"]["Bedwars"]["wins_bedwars"]
-            print("You have this many skywars wins:" + str(skywarsWins))
-            print("You have this many bedwars wins:" + str(bedwarsWins))
-        except:
-            print("Did you put the wrong username?")
+
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route('/ingamename',methods=["get","POST"])
-def data():
+@app.route('/data',methods=["get","POST"])
+def add():
     if request.method == "POST":
+        print(request.form.get("yess"))
         cursor = get_db().cursor()
-        uuid = requests.get("https://api.mojang.com/users/profiles/minecraft/lilycraft132").json()
-        profiles1 = requests.get("https://api.hypixel.net/player?key=9d49337e-bf1c-4825-84e4-71c529e778a3&uuid="+uuid["id"]).json()
+        uuid = requests.get("https://api.mojang.com/users/profiles/minecraft/"+request.form.get("yess")).json()
+        profiles1 = requests.get("https://api.hypixel.net/player?key=" + key + "&uuid="+uuid["id"]).json()
         profliedata = list(profiles1["player"]["stats"]["SkyBlock"]["profiles"].keys())[0]
-        data = requests.get("https://api.hypixel.net/skyblock/profile?key=9d49337e-bf1c-4825-84e4-71c529e778a3&profile="+profliedata).json()
-        purse = data["profile"]["members"]["f95ceea60a084921a25b22bc726fafaa"]["coin_purse"]
+        data = requests.get("https://api.hypixel.net/skyblock/profile?key="+ key + "&profile="+profliedata).json()
+        purse = data["profile"]["members"][uuid["id"]]["coin_purse"]
         bank = data["profile"]["banking"]["balance"]
-        sql = "INSERT INTO data (Purse,Bank) VALUES(?,?)"
-        cursor.execute(sql,(purse,bank))
-        print(request.form["ingamename"])
+        sql = "INSERT INTO data (Purse,Bank,UUID) VALUES(?,?,?)"
+        cursor.execute(sql,(purse,bank,uuid["id"]))
         get_db().commit()
-        return render_template("contents.html")
+        return redirect('/contents') 
+
+@app.route('/contents',methods=["get"])
+def data():
+    cursor = get_db().cursor()
+    sql = "SELECT max(Purse), max(Bank) FROM data"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return render_template("contents.html",results=results)
+
+ 
+
 
 if __name__ == "__main__":
     app.run(debug=True)
